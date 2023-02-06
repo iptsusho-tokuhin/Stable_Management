eadec395011f4187fa66ecb8842e9f6733b0d17b
iptsusho-tokuhin <87468971+iptsusho-tokuhin@users.noreply.github.com>
2023-02-06 11:23:01 +0900

    Update gas_javaf.js

--- js/gas_javaf.js
+++ js/gas_javaf.js
@@ -7,6 +7,8 @@ function Output_to_gas()
 	output[0] = data;
 	output[1] = note;
 	output[2] = his;
+	output[3][0][0] = document.getElementById('forum').value;
+	
 	
 	let DataJSON = JSON.stringify(output); 
 	google.script.run.withSuccessHandler(showStartes).write_ss(DataJSON);
@@ -58,6 +60,7 @@ function output_to_html(DataJSON)
 	data = input[0];
 	note = input[1];
 	his = input[2];
+	forum = input[3][0][0].replace(/<br>/g, "\n");
 	
 	set_data();
 	export_table();
