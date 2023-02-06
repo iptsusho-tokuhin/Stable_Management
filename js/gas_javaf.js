6634866bb760fc76094867feceef3eaf01e70909
iptsusho-tokuhin <87468971+iptsusho-tokuhin@users.noreply.github.com>
2023-02-06 11:05:09 +0900

    Update gas_javaf.js

--- js/gas_javaf.js
+++ js/gas_javaf.js
@@ -2,41 +2,47 @@ function Output_to_gas()
 {
 	document.getElementById('startes').innerHTML = "保存中";
 	var text = '';
+	var output;
 	
-	for(var i = 0; i < data.length; i++)
-	{
-		for(var j = 0; j < data[i].length; j++)
-		{
-				text += data[i][j] + ',';
-		}
-		text += '\n';
-	}
+	output[0] = data;
+	output[1] = note;
+	output[2] = his;
 	
-	text += '***';//データ区切り
+	let DataJSON = JSON.stringify(output); 
+	google.script.run.withSuccessHandler(showStartes).write_ss(DataJSON);
+	
+	//for(var i = 0; i < data.length; i++)
+	//{
+	//	for(var j = 0; j < data[i].length; j++)
+	//	{
+	//			text += data[i][j] + ',';
+	//	}
+	//	text += '\n';
+	//}
+	
+	//text += '***';//データ区切り
 
-	for(var i = 0; i < note.length; i++)
-	{
-		text += note[i] + ',' + '\n';
-	}
+	//for(var i = 0; i < note.length; i++)
+	//{
+	//	text += note[i] + ',' + '\n';
+	//}
 	
-	text += '***';//データ区切り
+	//text += '***';//データ区切り
 	
-	for(var i = 0; i < his.length; i++)
-	{
-		text += his[i] + ',' + '\n';
-	}
+	//for(var i = 0; i < his.length; i++)
+	//{
+	//	text += his[i] + ',' + '\n';
+	//}
 	
-	text += '***';//データ区切り
+	//text += '***';//データ区切り
 	
-	text += document.getElementById('forum').value.replace(/\r?\n/g, '<br>') + ',' + '\n';
+	//text += document.getElementById('forum').value.replace(/\r?\n/g, '<br>') + ',' + '\n';
 	
-	google.script.run.withSuccessHandler(showStartes).write_ss(text);
+	//google.script.run.withSuccessHandler(showStartes).write_ss(text);
 }
 
 function showStartes(returnString)
 {
-	//alert(returnString);
-	//Intput_from_gas();//動作お試し
 	document.getElementById('startes').innerHTML = returnString;
 }
 
@@ -46,32 +52,12 @@ function Intput_from_gas()
 	google.script.run.withSuccessHandler(output_to_html).read_ss();
 }
 
-function output_to_html(text)
+function output_to_html(DataJSON)
 {
-	//動作試験
-	var arr1 = text.split('***');
-	var arr2 = [];
-	var arr3 = [];
-	for(var i = 0; i < arr1.length; i++)
-	{
-		arr2[i] = arr1[i].split('\n');
-		arr2[i].pop();//各最後の行はごみデータになるので削除
-	}
-	
-	for(var i = 0; i < arr2.length; i++)
-	{
-		arr3[i] = [];
-		for(var j = 0; j < arr2[i].length; j++)
-		{
-			arr3[i][j] = arr2[i][j].split(',');
-			arr3[i][j].pop();//各最後の行はごみデータになるので削除
-		}
-	}
-	
-	data = arr3[0];
-	for(var i = 0; i < arr3[1].length; i++){note[i] = arr3[1][i][0];}
-	for(var i = 0; i < arr3[2].length; i++){his[i] = arr3[2][i][0];}
-	forum = arr3[3][0][0].replace(/<br>/g, "\n");
+	var input = JSON.parse(DataJSON);
+	data = input[0];
+	note = input[1];
+	his = input[2];
 	
 	set_data();
 	export_table();
@@ -80,4 +66,33 @@ function output_to_html(text)
 	set_select();
 	document.getElementById('forum').value = forum;
 	document.getElementById('startes').innerHTML = "読込完了";
+	
+	
+	
+	
+	
+	//動作試験
+	//var arr1 = text.split('***');
+	//var arr2 = [];
+	//var arr3 = [];
+	//for(var i = 0; i < arr1.length; i++)
+	//{
+	//	arr2[i] = arr1[i].split('\n');
+	//	arr2[i].pop();//各最後の行はごみデータになるので削除
+	//}
+	
+	//for(var i = 0; i < arr2.length; i++)
+	//{
+	//	arr3[i] = [];
+	//	for(var j = 0; j < arr2[i].length; j++)
+	//	{
+	//		arr3[i][j] = arr2[i][j].split(',');
+	//		arr3[i][j].pop();//各最後の行はごみデータになるので削除
+	//	}
+	//}
+	
+	//data = arr3[0];
+	//for(var i = 0; i < arr3[1].length; i++){note[i] = arr3[1][i][0];}
+	//for(var i = 0; i < arr3[2].length; i++){his[i] = arr3[2][i][0];}
+	//forum = arr3[3][0][0].replace(/<br>/g, "\n");
 }
